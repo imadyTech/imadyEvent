2ae106aad0d3e48ec0e056b1263bf73ee0ede6a1
DESKTOP-JDT5O80fs <fs@imady.com>
2022-06-08 13:38:51 +1200

    update.

--- IMadYEventSystem/IMadYProvider.cs
+++ IMadYEventSystem/IMadYProvider.cs
@@ -3,7 +3,7 @@ using imady.Message;
 
 namespace imady.Event
 {
-    public interface IIMadYProvider<T> : IMadYEventObjectBase where T : MadYMessageBase
+    public interface IMadYProvider<T> : IMadYEventObjectBase where T : MadYMessageBase
     {
         new void Subscribe(IMadYEventObjectBase observer);
 
